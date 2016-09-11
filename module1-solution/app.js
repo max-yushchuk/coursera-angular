4e9c9af31d2950df02c2eb600639b0c2b565c636
Max Yushchuk <max.yushchuk@gmail.com>
2016-09-12 00:31:50 +0300

    Add first assignment

---
+++ module1-solution/app.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+
+    var convertString = function(input) {
+      var inputArray = input.split(',');
+      var outputArray = [];
+      inputArray.forEach(function(item) {
+        item = item.trim();
+        if (item != '') outputArray.push(item);
+      });
+      return outputArray;
+    }
+
+    $scope.check = function(dishes) {
+      
+      if (dishes !== '' && dishes !== undefined) {
+        var dishes = convertString(dishes);
+
+        if (dishes.length <= 3) $scope.result = 'Enjoy!';
+        else $scope.result = 'Too much!';  
+      } else $scope.result = 'Please enter data first';
+    }
+  }
+})();
